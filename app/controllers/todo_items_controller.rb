c649a8b1f0d34c685ed13c4f78ffcf1c2b9e688b
davidjspencer <davidjspencer@gmail.com>
2015-11-10 14:16:27 +0100

    pass rq07a

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,35 +1,29 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_list
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
-
-  # GET /todo_items
-  # GET /todo_items.json
-  def index
-    @todo_items = TodoItem.all
-  end
-
-  # GET /todo_items/1
-  # GET /todo_items/1.json
+  
+  
   def show
   end
 
-  # GET /todo_items/new
+  
   def new
-    @todo_item = TodoItem.new
+    @todo_item = @todo_list.todo_items.new ### does it need parameters?
   end
 
-  # GET /todo_items/1/edit
+  
   def edit
   end
 
-  # POST /todo_items
-  # POST /todo_items.json
+  
+  
   def create
-    @todo_item = TodoItem.new(todo_item_params)
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
-        format.json { render :show, status: :created, location: @todo_item }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully created.' }
+        format.json { render :show, status: :created, location: @todo_list }
       else
         format.html { render :new }
         format.json { render json: @todo_item.errors, status: :unprocessable_entity }
@@ -37,13 +31,12 @@ class TodoItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todo_items/1
-  # PATCH/PUT /todo_items/1.json
+
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo_item }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @todo_list }
       else
         format.html { render :edit }
         format.json { render json: @todo_item.errors, status: :unprocessable_entity }
@@ -51,12 +44,11 @@ class TodoItemsController < ApplicationController
     end
   end
 
-  # DELETE /todo_items/1
-  # DELETE /todo_items/1.json
+  
   def destroy
     @todo_item.destroy
     respond_to do |format|
-      format.html { redirect_to todo_items_url, notice: 'Todo item was successfully destroyed.' }
+      format.html { redirect_to todo_list_todo_items_url, notice: 'Todo item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,7 +56,11 @@ class TodoItemsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_item
-      @todo_item = TodoItem.find(params[:id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
